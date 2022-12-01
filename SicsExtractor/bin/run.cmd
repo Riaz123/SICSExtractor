2cc2c49767e33478ce9765cb854acf5a563c5631
Riaz123 <riaz0609@gmail.com>
2022-12-01 12:39:23 +0000

    Delete type_of_business.sql

--- SicsExtractor/bin/run.cmd
+++
@@ -1,3 +0,0 @@
-@SETLOCAL
-SET PYTHONPATH=%~dp0..\dataapp
-python %PROJ_DIR%dataapp\runner.py
--- SicsExtractor/sql/gpi3/model_group_model.sql
+++
@@ -1,5 +0,0 @@
-SELECT mgm.Id,
-       mgm.ModelGroupId,
-       mgm.ModelId,
-       mgm.IsNew
-FROM [dbo].ModelGroupModel mgm
